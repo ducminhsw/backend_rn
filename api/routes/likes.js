29a61ec614dadb5b7efd56b0badc88c64d3e291a
Thanh Ta <borntobeagymer7@gmail.com>
2023-02-06 02:15:31 +0700

    --update_backend

--- api/routes/likes.js
+++ api/routes/likes.js
@@ -1,26 +1,22 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
 const verify = require('../utils/verifyToken');
+const { getUserIDFromToken } = require("../utils/getUserIDFromToken");
 var {responseError, setAndSendResponse} = require('../response/error');
-
 router.post('/like', verify, async (req, res) => {
-    var {id} = req.query;
-    var user = req.user;
-
+    var {id,token} = req.query;
+    var user = await getUserIDFromToken(token);
     // PARAMETER_IS_NOT_ENOUGH
     if(id !== 0 && !id) {
         console.log("No have parameter id");
         return setAndSendResponse(res, responseError.PARAMETER_IS_NOT_ENOUGH);
     }
-
     // PARAMETER_TYPE_IS_INVALID
     if((id && typeof id !== "string")) {
         console.log("PARAMETER_TYPE_IS_INVALID");
         return setAndSendResponse(res, responseError.PARAMETER_TYPE_IS_INVALID);
     }
-
     var post;
-
     try {
         post = await Post.findById(id);
     } catch (err) {
@@ -35,19 +31,17 @@ router.post('/like', verify, async (req, res) => {
         console.log("Post is not existed");
         return setAndSendResponse(res, responseError.POST_IS_NOT_EXISTED);
     }
-
     if(post.likedUser) {
-        if(post.likedUser.includes(user.id)) {
-            const index = post.likedUser.indexOf(user.id);
+        if(post.likedUser.includes(user._id)) {
+            const index = post.likedUser.indexOf(user._id);
             post.likedUser.splice(index, 1);
         } else {
-            post.likedUser.push(user.id);
+            post.likedUser.push(user._id);
+            // console.log(post.likedUser)
         }
     } else {
-        post.likedUser = [user.id];
+        post.likedUser = [user._id];
     }
-
-
     try {
         const updatedPost = await post.save();
         res.status(200).send({
